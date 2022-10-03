5e8b70e0abc0f8f57bce65e809e5327b3ed4ce21
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-10-03 14:18:08 +0300

    [testing] Optional target name. Improved docs

--- icm_testing.cmake
+++ icm_testing.cmake
@@ -25,7 +25,7 @@
 #
 #           VERSION HISTORY
 #
-#   1.03 (2022-10-01) Labels support
+#   1.03 (2022-10-03) Labels support. Optional target name
 #   1.02 (2022-07-02) Removed doctest macro. Moved to another lib (doctest-util)
 #   1.01 (2021-11-27) Namespace doctest lib tests with library name. Docs
 #   1.00 (2020-12-25) Initial standalone release
@@ -35,15 +35,29 @@ include_guard(GLOBAL)
 # icm_add_test
 #
 # Create an executable target and add it as a test
+#
 # Args:
-#   * NAME name       - name of the test (visible when ctest is run)
-#   * TARGET target   - name of the executable
-#   * SOURCES sources - sources for the executable
-#   * LIBRARIES libs  - libraries to link with
-#   * LABELS          - test labels
-#   * FOLDER folder   - MSVC solution folder for the target
+#   * NAME name       - Name of the test (visible when ctest is run)
+#   * TARGET target   - Optional. Name of the test executable.
+#                       Defaults to '<name>-test' if not provided
+#   * SOURCES sources - Sources for the executable
+#   * LIBRARIES libs  - Optional. Link libraries for the executable
+#   * LABELS labels   - Optional. CTest labels for the test
+#   * FOLDER folder   - Optional. MSVC solution folder for the target
+#
+# Example:
+# icm_add_test(
+#   NAME mylib-core
+#   LIBRARIES mylib
+#   SOURCES
+#       mylib-test-helpers.cpp
+#       mylib-core-tests.cpp
+# )
 macro(icm_add_test)
     cmake_parse_arguments(ARG "" "NAME;TARGET;FOLDER" "SOURCES;LIBRARIES;LABELS" ${ARGN})
+    if(NOT DEFINED ARG_TARGET)
+        set(ARG_TARGET "${ARG_NAME}-test")
+    endif()
     add_executable(${ARG_TARGET} ${ARG_SOURCES})
     if(DEFINED ARG_LIBRARIES)
         target_link_libraries(${ARG_TARGET} PRIVATE ${ARG_LIBRARIES})
