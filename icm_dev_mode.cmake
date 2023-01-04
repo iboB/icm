cea9b16b4cf2eda39fe2d64c5facc086db5bd5f4
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-01-04 16:06:55 +0200

    [dev_mode] NOMIMAX for msvc

--- icm_dev_mode.cmake
+++ icm_dev_mode.cmake
@@ -89,7 +89,7 @@ if(MSVC)
     # /Zc:preprocessor - incompatible with Windows.h
     # /Zc:templateScope - TODO: add when msvc 17.5 is the norm
     set(icm_compiler_flags "-D_CRT_SECURE_NO_WARNINGS /Zc:__cplusplus /permissive-\
-        /volatile:iso /Zc:throwingNew /utf-8\
+        /volatile:iso /Zc:throwingNew /utf-8 -DNOMINMAX=1\
         /w34100 /w34189 /w34701 /w34702 /w34703 /w34706 /w34714 /w34913\
         /wd4251 /wd4275"
     )
