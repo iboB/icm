5cbfc14d0e64223ba5d4a931bef3858775e354d8
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-09-08 15:08:29 +0300

    [dev_mode] /Zc:templateScope for msvc, minor reorder

--- icm_dev_mode.cmake
+++ icm_dev_mode.cmake
@@ -25,6 +25,8 @@
 #
 #           VERSION HISTORY
 #
+#   1.10 (2023-07-13) /Zc:templateScope for MSVC
+#                     minor code reorder
 #   1.09 (2023-07-13) Set USE_FOLDERS to ON
 #   1.08 (2023-04-29) Improved (idiomatic) setting of compiler options
 #   1.07 (2023-03-20) Improved sanitizer support: finer grain selection
@@ -79,6 +81,7 @@ endif()
 
 set(ICM_DEV_MODE ON)
 
+# standard
 if (NOT ICM_DEV_CXX_STANDARD)
     set(ICM_DEV_CXX_STANDARD 17)
 endif()
@@ -93,22 +96,17 @@ set(CMAKE_C_STANDARD ${ICM_DEV_C_STANDARD})
 set(CMAKE_C_EXTENSIONS OFF)
 set(CMAKE_C_STANDARD_REQUIRED ON)
 
-set(CMAKE_LINK_DEPENDS_NO_SHARED ON)
-
-set_property(GLOBAL PROPERTY USE_FOLDERS ON)
-
-option(SAN_THREAD "${CMAKE_PROJECT_NAME}: sanitize thread" OFF)
-option(SAN_ADDR "${CMAKE_PROJECT_NAME}: sanitize address" OFF)
-option(SAN_UB "${CMAKE_PROJECT_NAME}: sanitize undefined behavior" OFF)
-option(SAN_LEAK "${CMAKE_PROJECT_NAME}: sanitize leaks" OFF)
+# misc config
+set(CMAKE_LINK_DEPENDS_NO_SHARED ON) # only relink exe is so interface changes
+set_property(GLOBAL PROPERTY USE_FOLDERS ON) # use solution folders
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin) # binaries to bin
 
 if(MSVC)
     # /Zc:preprocessor - incompatible with Windows.h
-    # /Zc:templateScope - TODO: add when msvc 17.5 is the norm
     add_compile_options(
         /W4
         -D_CRT_SECURE_NO_WARNINGS /Zc:__cplusplus /permissive-
-        /volatile:iso /Zc:throwingNew /utf-8 -DNOMINMAX=1
+        /volatile:iso /Zc:throwingNew /Zc:templateScope /utf-8 -DNOMINMAX=1
         /wd4251 /wd4275
     )
 else()
@@ -116,6 +114,11 @@ else()
 endif()
 
 # sanitizers
+option(SAN_THREAD "${CMAKE_PROJECT_NAME}: sanitize thread" OFF)
+option(SAN_ADDR "${CMAKE_PROJECT_NAME}: sanitize address" OFF)
+option(SAN_UB "${CMAKE_PROJECT_NAME}: sanitize undefined behavior" OFF)
+option(SAN_LEAK "${CMAKE_PROJECT_NAME}: sanitize leaks" OFF)
+
 if(MSVC)
     if(SAN_ADDR)
         add_compile_options(/fsanitize=address)
@@ -149,6 +152,3 @@ else()
         add_link_options(${icm_san_flags})
     endif()
 endif()
-
-# all binaries to bin
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
