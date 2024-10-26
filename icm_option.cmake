5f2db8e265e66d404cd32d93c4d491c1226da06a
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-10-26 08:34:54 +0300

    [option] icm_auto_option: add MSG_STATUS

--- icm_option.cmake
+++ icm_option.cmake
@@ -38,24 +38,35 @@
 #   * default - default value of the option (AUTO, ON, OFF)
 #
 # AUTO should be interpreted as letting the script or build system decide.
-# The macro also defines the var <option>_REQUIRED which is either empty or
-# REQUIRED. Besides in `if` checks, it can be used to conditionally require a
-# package or feature
+# The macro also defines two helper options:
 #
-# Example:
-# icm_auto_option(USE_SSL "myproj: use SSL" AUTO)
-# set(have_ssl FALSE)
-# if(USE_SSL)
-#     # error if USE_SSL is set to ON, but not an eror if it's set to AUTO
-#     find_package(OpenSSL ${USE_SSL_REQUIRED})
-#     if(NOT OpenSSL_FOUND)
-#         message(STATUS "myproj not using SSL")
-#     else()
-#         message(STATUS "myproj using SSL")
-#         set(have_ssl TRUE)
-#     endif()
-# endif()
+# * <option>_REQUIRED which is either empty or REQUIRED.
+#   Besides in `if` checks, it can be used to conditionally require a
+#   package or feature
 #
+#   Example:
+#   icm_auto_option(USE_SSL "myproj: use SSL" AUTO)
+#   set(have_ssl FALSE)
+#   if(USE_SSL)
+#       # error if USE_SSL is set to ON, but not an eror if it's set to AUTO
+#       find_package(OpenSSL ${USE_SSL_REQUIRED})
+#       if(NOT OpenSSL_FOUND)
+#           message(STATUS "myproj not using SSL")
+#       else()
+#           message(STATUS "myproj using SSL")
+#           set(have_ssl TRUE)
+#       endif()
+#   endif()
+#
+# * <option>_MSG_STATUS which is either STATUS or FATAL_ERROR.
+#   It can be used to conditionally print a message or error
+#
+#   Example:
+#   icm_auto_option(BUILD_GUI_EXAMPLE "myproj: build GUI example" AUTO)
+#   if(WIN32)
+#       message(${BUILD_GUI_EXAMPLE_MSG_STATUS}
+#               "myproj: GUI example is not supported on Windows")
+#   endif()
 
 set(icm_auto_option_valid_values AUTO ON OFF TRUE FALSE YES NO)
 
@@ -72,7 +83,9 @@ macro(icm_auto_option var desc default)
     if(NOT ${var} OR ${${var}} STREQUAL AUTO)
         # not required
         set(${var}_REQUIRED)
+        set(${var}_MSG_STATUS STATUS)
     else()
         set(${var}_REQUIRED REQUIRED)
+        set(${var}_MSG_STATUS FATAL_ERROR)
     endif()
 endmacro()
