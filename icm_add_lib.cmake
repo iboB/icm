d4928e3d2394bdf67889056eaaa0a4e47cc7ee90
Borislav Stanimirov <b@ibob.bg>
2024-11-13 12:09:50 +0200

    [add_lib]: add icm_add_module and icm_set_visibility_hidden

--- icm_add_lib.cmake
+++ icm_add_lib.cmake
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 # MIT License:
-# Copyright (c) 2020-2022 Borislav Stanimirov
+# Copyright (c) 2020-2024 Borislav Stanimirov
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files(the
@@ -25,12 +25,22 @@
 #
 #           VERSION HISTORY
 #
+#   1.04 (2024-11-13) Add icm_add_module and icm_set_visibility_hidden
 #   1.03 (2023-11-24) Use BUILD_SHARED_LIBS as opposed to ICM_STATIC_LIBS
 #   1.02 (2023-10-04) Add -fvisibility-inlines-hidden for *nix builds
 #   1.01 (2022-10-05) Macros to functions
 #   1.00 (2020-12-25) Initial standalone release
 #
 include_guard(GLOBAL)
+
+function(icm_set_visibility_hidden target)
+    if(NOT WIN32)
+        target_compile_options(${target} PRIVATE
+            -fvisibility=hidden
+            -fvisibility-inlines-hidden
+        )
+    endif()
+endfunction()
 
 # icm_add_shared_lib
 #
@@ -40,7 +50,7 @@ include_guard(GLOBAL)
 #   * target - name of the target (the lib we're adding)
 #   * cname  - a name to be used when for the definitons added must be a valid
 #              C/C++ symbol name
-#   * ...    - sources
+#   * ...    - optional sources
 # Definitions added to target when cname is "MYLIB":
 #   * BUILDING_MYLIB=1
 #   * MYLIB_SHARED=1
@@ -58,12 +68,7 @@ include_guard(GLOBAL)
 # #endif
 function(icm_add_shared_lib target cname)
     add_library(${target} SHARED ${ARGN})
-    if(NOT WIN32)
-        target_compile_options(${target} PRIVATE
-            -fvisibility=hidden
-            -fvisibility-inlines-hidden
-        )
-    endif()
+    icm_set_visibility_hidden(${target})
 
     target_compile_definitions(${target}
         PRIVATE -DBUILDING_${cname}=1
@@ -87,5 +92,24 @@ function(icm_add_lib target cname)
         add_library(${target} STATIC ${ARGN})
     else()
         icm_add_shared_lib(${target} ${cname} ${ARGN})
+    endif()
+endfunction()
+
+# icm_add_module
+#
+# icm_add_module(mylib mylib_a.cpp mylib_b.cpp [APPLE_FORCE_DYLIB])
+# Adds a module library, forces visibility hidden
+# Args:
+#   * target - name of the target (the lib we're adding)
+#   * ...    - optional sources
+#   * APPLE_FORCE_DYLIB - if present, forces the module to be a .dylib on Apple.
+#     By default on Apple platforms the default suffix by CMake for modules is
+#     `so`. This doesn't play well with many loaders which require .dylib files.
+function(icm_add_module target)
+    cmake_parse_arguments(ICM "APPLE_FORCE_DYLIB" "" "" ${ARGN})
+    add_library(${target} MODULE ${ARGN})
+    icm_set_visibility_hidden(${target})
+    if(APPLE AND ICM_APPLE_FORCE_DYLIB)
+        set_target_properties(${target} PROPERTIES SUFFIX ".dylib")
     endif()
 endfunction()
